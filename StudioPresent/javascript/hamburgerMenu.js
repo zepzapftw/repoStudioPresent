458ca76538387fcf5d213edee65ae0472b8c39f3
Bogdan <bogdansombor@gmail.com>
2024-07-04 12:40:27 +0200

    promena6

---
+++ StudioPresent/javascript/hamburgerMenu.js
@@ -0,0 +1,10 @@
+function hamburger(){
+    let x=document.getElementById("HambugerLinks");
+    
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }  
+}
